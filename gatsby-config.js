6df7ee0427b1163b3f38f462466bb4ccfff67735
ChaitanyaKhoje <chaitanyakhoje9@gmail.com>
2020-04-05 21:06:56 -0700

    hello

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'ChaitanyaKhoje',
-        short_name: 'ChaitanyaKhoje',
+        name: 'BrittanyChiang',
+        short_name: 'BrittanyChiang',
         start_url: '/',
         background_color: config.darkNavyColor,
         theme_color: config.navyColor,
